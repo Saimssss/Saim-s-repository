6cec28a35427f72fab28a6f6061ff272e9d476b7
Saimssss <samuelesteban.santander@gmail.com>
2025-09-11 17:59:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ const next = document.querySelector(".next")
 let index = 0;
 
 
-
-// actualizar el desplazamiento del slider
-function updateSlider() {
-    const slideWidth = slides.children[0].clientWidth;
-    slides.style.transform = `translateX(-${index * slideWidth}px)`;
-}
 // mover a la diapositiva anterior
 prev.addEventListener("click", () => {
     index = (index > 0) ? index - 1 : slides.children.length - 1;
@@ -24,6 +18,13 @@ next.addEventListener("click", () => {
     updateSlider();
 });
 
+// actualizar el desplazamiento del slider
+function updateSlider() {
+    const slideWidth = slides.children[0].clientWidth;
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
 
 // adjustar el tamaño al cambiar el tamaño de la ventana
 window.addEventListener("resize", updateSlider)
+
